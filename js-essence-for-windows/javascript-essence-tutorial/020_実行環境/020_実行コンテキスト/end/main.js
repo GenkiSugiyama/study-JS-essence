85445fa0cb7461f1572d214f456cb3801b64945f
sugiyama-g <sugiyama-g@ad-win.jp>
2020-11-27 08:48:52 +0900

    session4：実行コンテキスト

--- js-essence-for-windows/javascript-essence-tutorial/020_実行環境/020_実行コンテキスト/end/main.js
+++ js-essence-for-windows/javascript-essence-tutorial/020_実行環境/020_実行コンテキスト/end/main.js
@@ -1,7 +1,10 @@
 let a = 0;
 function b() {
+    // 関数コンテキスト：関数実行中に呼び出されるコンテキスト
     console.log(this, arguments, a);
 }
 
 // console.log(a);
 b();
+// argumentsは関数コンテキストで用意されるのでグローバルな場所では使用できない（エラーになる）
+console.log(arguments)
