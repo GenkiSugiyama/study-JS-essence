54104c5793ecd3e498d3135746d4649a285d9f52
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-12-22 00:20:57 +0900

    session7：this

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/050_関数とオブジェクト１/040_this/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/050_関数とオブジェクト１/040_this/start/main.js
@@ -1,6 +1,10 @@
+// this：呼び出し元のオブジェクトへの参照を保持するキーワード
 const person = {
     name: 'Tom',
     hello: function() {
-
+        // 呼び出し元のオブジェクト（person）を参照したいのでthisを使う
+        console.log("Hello " + this.name);
     }
 }
+
+person.hello();
