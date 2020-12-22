fed255ec2f1639c4a671f3e411b8a9f88a72d217
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-12-23 01:25:11 +0900

    session7：アロー関数とthis

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
@@ -1,9 +1,39 @@
 window.name = 'John';
 
-const person = {
+// const a = () => {
+//     console.log('Bye ' + this.name);
+// };
+
+// var person = {
+//     name: 'Tom',
+//     // 通常の無名関数だとhelloメソッド内のthisは呼び出し元のオブジェクトを参照するが
+//     // アロー関数に変更するとグローバルオブジェクトへの参照となる
+//     // アロー関数でのthisは
+//     hello: function() {
+//         console.log('Hello ' + this.name);
+//         // aの宣言時のレキシカルスコープはグローバルスコープのwindowとなる
+//         // そのためa内のthisw.nameはwindow.nameと同じになる
+//         a();
+//     }
+// }
+// person.hello();
+
+var person = {
     name: 'Tom',
+    // 通常の無名関数だとhelloメソッド内のthisは呼び出し元のオブジェクトを参照するが
+    // アロー関数に変更するとグローバルオブジェクトへの参照となる
+    // アロー関数でのthisは
     hello: function() {
         console.log('Hello ' + this.name);
+        // この場合aの宣言はhelloメソッド内で行われている
+        // この時のレキシカルスコープはpersonとなるためa内のthis.nameはperson.nameとなる
+        const a = () => {
+            console.log('Bye ' + this.name);
+        };
+        // aの宣言時のレキシカルスコープはグローバルスコープのwindowとなる
+        // そのためa内のthisw.nameはwindow.nameと同じになる
+        a();
     }
 }
 person.hello();
+
