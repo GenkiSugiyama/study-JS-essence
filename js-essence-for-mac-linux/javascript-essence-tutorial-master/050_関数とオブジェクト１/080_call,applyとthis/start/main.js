4f24cd759fa6a108bef9eab5e4bc95326f89b408
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-12-23 00:41:18 +0900

    session7：call,applyとthis

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -1,7 +1,33 @@
-function a(name) {
+// bind：thisや引数の参照先を固定した新しい関数を作成。bind使用時点ではその関数は実行されない
+
+// call, apply：thisや引数の参照先を変更する点ではbindと同じ？このメソッドを使用した時点で作成された関数を実行する
+
+function a(name, name1) {
     console.log('hello ' + name);
 }
+
+// bindなどの引数となるオブジェクトを変数に格納
+const tim = {name: 'Tim'};
 
 const b = a.bind(null, 'Tim');
 
 b();
+
+function c() {
+    console.log('hello ' + this.name);
+}
+
+const d = c.bind({name: 'Tom'});
+
+d();
+
+// apply：第二引数には配列を渡し、関数の仮引数に順番に渡す
+// 複数の引数に対して、値を1つずつ渡したい場合に使用する（ES6以降はスプレッド演算子）
+a.apply(tim, ['Tim', 'Tom', 'John']);
+
+// call：第一引数にはthis、第二引数以降には仮引数を指定
+c.call(tim);
+
+// applyの実例
+const array = [1,2,3,4,5];
+
