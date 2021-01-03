23c39706299e884a37de5df934a927460ab586ed
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-04 02:11:26 +0900

    session8：new演算子（完了）

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -3,7 +3,10 @@
 // call, apply：thisや引数の参照先を変更する点ではbindと同じ？このメソッドを使用した時点で作成された関数を実行する
 
 function a(name, name1) {
-    console.log('hello ' + name);
+    this.name1 = "a",
+    this.name2 = "b",
+    this.name3 = "c"
+    console.log('hello ' + this.name1);
 }
 
 // bindなどの引数となるオブジェクトを変数に格納
@@ -11,7 +14,7 @@ const tim = {name: 'Tim'};
 
 const b = a.bind(null, 'Tim');
 
-b();
+// b();
 
 function c() {
     console.log('hello ' + this.name);
@@ -19,14 +22,14 @@ function c() {
 
 const d = c.bind({name: 'Tom'});
 
-d();
+// d();
 
 // apply：第二引数には配列を渡し、関数の仮引数に順番に渡す
 // 複数の引数に対して、値を1つずつ渡したい場合に使用する（ES6以降はスプレッド演算子）
 a.apply(tim, ['Tim', 'Tom', 'John']);
 
 // call：第一引数にはthis、第二引数以降には仮引数を指定
-c.call(tim);
+// c.call(tim);
 
 // applyの実例
 const array = [1,2,3,4,5];
--- js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/030_new演算子/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/030_new演算子/start/main.js
@@ -1,18 +1,36 @@
 function F(a, b) {
   this.a = a;
   this.b = b;
-  // コンストラクター関数のreturn値がオブジェクトの場合は
-  // インスタンス化されたオブジェクトににはreturnの値を返す
-  // return {}; →この場合はinstanceを確認すると空のオブジェクトが格納されている
-  // returnの値がオブジェクト以外or未定義の場合はコンストラクター関数内のprototypeを__proto__にコピーする&インスタンスオブジェクトをthisの参照先としてコンストラクター関数を実行する
-  // return 1; →thisに格納されたプロパティを持ったオブジェクトが生成される
+  this.hello = function() {
+    console.log('hello');
+  }
+  // コンストラクター関数のreturn値がオブジェクトの場合はreturnの値を持つインスタンスが生成される
+  return {}; // →この場合はinstanceを確認すると空のオブジェクトが格納されている
+  // returnの値がオブジェクト以外or未定義の場合はコンストラクター関数内のprototypeを__proto__にコピーする&コンストラクター関数内で定義したプロパティ、メソッドを持つインスタンスが生成される
+  // return 1; // →thisに格納されたプロパティを持ったオブジェクトが生成される
 }
 
 F.prototype.c = function(){};
 
-// const instance = new F(1, 2);
-console.log(instance);
+const instance = new F(1,2);
+// console.log(instance);
 
+function newOpe(C, ...args) {
 
-function newOpe()
+  // Object.create：第一引数に指定したオブジェクトを__proto__にコピーした空のオブジェクトを生成する
+  const _this = Object.create(C.prototype);
+  // console.log(_this);
+
+  const result = C.apply(_this, args)
+  console.log(result, _this);
+
+  // コンストラクター関数の戻り値がObjectの場合、そのまま戻り値を返す
+  if(typeof result === "object" && result !== null) {
+    return result;
+  }
+
+  // コンストラクター関数の戻り値がObjectもしくは未定義の場合、thisで定めたプロパティ、メソッドを持つ＆prototypeを__proto__にコピーされたオブジェクトを返す
+  return _this;
+}
 const instance1 = newOpe(F, 1, 2);
+console.log(instance1);
