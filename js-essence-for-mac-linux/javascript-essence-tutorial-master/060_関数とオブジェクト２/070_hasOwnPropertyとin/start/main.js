b0feed8d2b0b6452ea65dce2ef4ab78af06eec4a
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-04 16:53:57 +0900

    session8：hasOwnPropertyとin

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
@@ -8,3 +8,8 @@ Object.prototype.hello = function() {
 }
 
 const bob = new Person('Bob', 18);
+// hasOwnProperty：あるプロパティをオブジェクト自身が持っているかどうかを判定する（__proto__以下のメソッドはfalse扱い）
+const resultOwn = bob.hasOwnProperty("hello"); // オブジェクト自身ではなく__proto__内で継承したメソッドのためfalse
+console.log(resultOwn)
+// in ：あるプロパティをオブジェクトが持つ or __proto__に含まれているかを判定する（プロトタイプチェーンを遡ってプロパティを探す）
+console.log("hello" in bob); // __proto__内で所有しているのでtrue
