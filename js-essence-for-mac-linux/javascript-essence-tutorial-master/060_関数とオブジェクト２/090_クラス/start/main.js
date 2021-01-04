e8068fbe17efd01446a6632ffc081a97d24dd1bd
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-04 17:51:32 +0900

    session8：クラス

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/090_クラス/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/090_クラス/start/main.js
@@ -1,8 +1,24 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
+// クラス：コンストラクター関数をクラス表記でかけるようにしたもの、ES6からこの表記ができるようになった
+class Person {
+  // コンストラクター関数内で定義したプロパティはconstructor関数を使って定義する
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+  // prototypeに定義したメソッドはそのままクラス内で定義する
+  hello() {
+    console.log('hello ' + this.name);
+  }
 }
+const bob = new Person('Bob', 23);
+
+// ↑クラス表記に変更
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// Person.prototype.hello = function() {
+//   console.log('hello ' + this.name);
+// }
