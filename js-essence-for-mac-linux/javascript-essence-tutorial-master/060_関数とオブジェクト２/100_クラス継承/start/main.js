65a3b8b3338539917be8511999154a08df12bef5
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-04 17:51:41 +0900

    session8：クラス継承

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/100_クラス継承/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/100_クラス継承/start/main.js
@@ -1,25 +1,56 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// Person.prototype.hello = function() {
+//   console.log('hello ' + this.name);
+// }
+// ↓クラス化
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  hello() {
+    console.log("hello " + this.name);
+  }
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+// クラス記法での継承は「class 継承先クラス extends 継承元クラス」と表記する
+// extendsで継承元クラスのprototypeまでコピーされる
+class Japanese extends Person {
+  constructor(name, age, gender) {
+    // 初期化のコンストラクターで継承元をそのまま使うときはsuperメソッドを使用する
+    super(name, age);
+    this.gender = gender;
+  }
+
+  hello() {
+    console.log('Konnichiwa ' + this.name);
+  }
+
+  bye() {
+    console.log('Sayonara ' + this.name);
+  }
 }
 
-function Japanese(name, age, gender) {
-  Person.call(this, name, age);
-  this.gender = gender;
-}
+// function Japanese(name, age, gender) {
+//   Person.call(this, name, age);
+//   this.gender = gender;
+// }
 
-Japanese.prototype = Object.create(Person.prototype);
+// Japanese.prototype = Object.create(Person.prototype);
 
-Japanese.prototype.hello = function() {
-  console.log('Konnichiwa ' + this.name);
-}
+// Japanese.prototype.hello = function() {
+//   console.log('Konnichiwa ' + this.name);
+// }
 
-Japanese.prototype.bye = function() {
-  console.log('Sayonara ' + this.name);
-}
+// Japanese.prototype.bye = function() {
+//   console.log('Sayonara ' + this.name);
+// }
 
 const taro = new Japanese('Taro', 23, 'Male');
+console.log(taro);
+taro.bye();
