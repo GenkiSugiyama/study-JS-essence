c252c55fd04c7c40c82a577ec65f4718936a1a2f
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-04 17:51:48 +0900

    session8：super

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/110_super/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/110_super/start/main.js
@@ -1,3 +1,5 @@
+// super：継承元の関数を呼び出すためのキーワード
+// 関数リテラル（波カッコ内）でのみ使用可能
 class Person {
     constructor(name, age) {
         this.name = name;
@@ -11,11 +13,15 @@ class Person {
 
 class Japanese extends Person {
     constructor(name, age, gender) {
+        // superは継承先クラスのconstructor関数内で継承元のコンストラクターを呼び出すためによく使われる
+        // その際、継承先クラスのthisに新しいプロパティをsuperより先に設定しようとするとエラーとなる
+        // superによる継承元のconstructorの呼び出しは最初に行う
         super(name, age);
         this.gender = gender;
     }
 
     hello() {
+        // 「super.関数」で継承元の関数を呼び出すこともできる
         super.hello();
         console.log('Konnichiwa ' + this.name);
     }
@@ -26,18 +32,31 @@ class Japanese extends Person {
 }
 
 const taro = new Japanese('Taro', 23, 'Male');
-console.log(taro);
-taro.bye();
+// console.log(taro);
+// taro.hello();// hello~とKonnichiwa~が表示される
 
-// const american = {
-//     hello() {
-//         console.log('hello ' + this.name);
-//     }
-// }
+const american = {
+    hello() {
+        console.log('hello ' + this.name);
+    }
+}
 
-// const bob = {
-//     name: 'Bob',
-//     hello() {
-//         super.hello();
-//     }
-// }
+const bob = {
+    name: 'Bob',
+    // bob内のhelloメソッドを無くしてもhelloメソッド使用可能
+    hello() {
+        // console.log('hello ' + this.name);
+        // setPrototypeOfによりamericanをprototypeとして継承したのでsuperを使って継承元のメソッドを呼び出している
+        super.hello();
+    },
+
+    bye() {
+        super.hello();
+        console.log('bye');
+    }
+}
+
+// オブジェクトをprototypeとして継承することで他オブジェクトのメソッドをsuperを使って呼び出せるようになる
+Object.setPrototypeOf(bob, american);
+
+bob.bye();
