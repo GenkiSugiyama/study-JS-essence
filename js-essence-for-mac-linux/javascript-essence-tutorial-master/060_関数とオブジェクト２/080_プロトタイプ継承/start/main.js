4dc3924b743bf09294bf23083ecac3cca0f3e2b8
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-04 16:54:07 +0900

    session8：プロトタイプ継承

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -1,3 +1,6 @@
+// プロトタイプ継承：別のコンストラクター関数のプロトタイプを受け継いで機能を流用できるようにすること
+// コンストラクター関数間でプロトタイプを共有できる
+// 継承：別のコンストラクター関数をそのまま受け継ぐこと
 function Person(name, age) {
   this.name = name;
   this.age = age;
@@ -6,3 +9,25 @@ function Person(name, age) {
 Person.prototype.hello = function() {
   console.log('hello ' + this.name);
 }
+
+function Japanese(name, age) {
+  // 継承
+  // 継承元のthisを継承先オブジェクト内のthisで束縛し、引数には継承先コンストラクター関数に必要な引数を指定する
+  Person.call(this, name, age);
+}
+
+// プロトタイプ継承
+// createメソッドを使ってあるコンストラクター関数のprototypeを別のコンストラクター関数に継承させる
+Japanese.prototype = Object.create(Person.prototype);
+
+const person = new Person("Tom", 18);
+console.log(person.name);
+person.hello();
+
+// プロトタイプ継承だけ行った場合は、prototypeオブジェクト内で定義されたメソッドは使用できるが
+// 継承元オブジェクトのプロパティは継承できていないのでnameやageはundefinedとなる
+const japanese = new Japanese("genki", 22);
+console.log(japanese);
+console.log(japanese.name);
+japanese.hello();
+
