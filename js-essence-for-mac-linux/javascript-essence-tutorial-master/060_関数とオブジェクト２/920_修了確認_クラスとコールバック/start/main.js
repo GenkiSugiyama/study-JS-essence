bf743b3cbf039d156f73d64b3ad49ee69ba49201
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-06 00:27:02 +0900

    session8：【修了確認】クラスとコールバック

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
@@ -16,8 +16,19 @@ class Person {
 
   hello() {
       console.log('hello ' + this.name);
+      return `hello ${this.name}`
   }
 }
 
 const bob = new Person('Bob', 23);
-setTimeout(bob.hello, 1000);
+
+// 1.bindでthisをbobで再度固定した上で実行する
+setTimeout(bob.hello.bind(bob), 2000);
+
+// ×：コールバック関数として呼ばれた場合のthisは親関数実行時のレキシカルスコープとなり、setTimeout実行時のレキシはwindowオブジェクトなのでwindowオブジェクトのプロパティにnameを加える → window.name = "Bob";
+// ○：オブジェクトのメソッドはコールバック関数内で呼ばれた場合オブジェクト経由の実行ではなく、関数としての実行となるのでthisの参照元がオブジェクトではなくなる
+// →自身の中でオブジェクト経由のhelloメソッドを実行する無名関数をsetTimeoutの第一引数として渡す
+setTimeout(function() {
+  return bob.hello();
+}, 1000);
+
