36a8fe071773e23658d6ac4edc92dbce704ddc3b
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-06 01:14:55 +0900

    session8；【修了問題】チェーンメソッド

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
@@ -24,12 +24,106 @@
  * 	.set(6) -> '42'を出力（10 - 3) * 6
  */
 
+ class Calculator {
+	constructor() {
+		// this.num;
+		// this.operator;
+
+		this.val = null;
+		this._operator;
+	}
+
+	set(num) {
+		// if(this.operator) {
+		// 	result = `${this.num} ${this.operator} ${num}`;
+		// 	console.log(result);
+		// 	return result;
+		// } else {
+		// 		console.log(num);
+		// 		return num;
+		// }
+
+		if(this.val === null) {
+			this.val = num;
+		} else {
+			this._operator(this.val, num);
+		}
+		return this;
+	}
+
+	plus() {
+		// this.operator = "+";
+		// return this.operator;
+		this._operator = function(val1, val2) {
+			const result = val1 + val2;
+			this._equal(result);
+		}
+		return this;
+	}
+
+	minus() {
+		// this.operator = "+";
+		// return this.operator;
+		this._operator = function(val1, val2) {
+			const result = val1 - val2;
+			this._equal(result);
+		}
+		return this;
+	}
+
+	multiply() {
+		// this.operator = "+";
+		// return this.operator;
+		this._operator = function(val1, val2) {
+			const result = val1 * val2;
+			this._equal(result);
+		}
+		return this;
+	}
+
+	divide() {
+		// this.operator = "+";
+		// return this.operator;
+		this._operator = function(val1, val2) {
+			const result = val1 / val2;
+			this._equal(result);
+		}
+		return this;
+	}
+
+	_equal(result) {
+		this.val = result;
+		console.log(result)
+	}
+
+	// minus() {
+	// 	// this.operator = "-";
+	// 	// return this.operator;
+
+	// 	return this;
+	// }
+
+	// multiply() {
+	// 	// this.operator = "*";
+	// 	// return this.operator;
+
+	// 	return this;
+	// }
+
+	// multiply() {
+	// 	// this.operator = "/";
+	// 	// return this.operator;
+
+	// 	return this;
+	// }
+ }
+
 const calc = new Calculator();
 
 calc.set(10)
 	.minus()
 	.set(3)
-	.mutiply()
+	.multiply()
 	.set(6)
 	.divide()
 	.set(2)
