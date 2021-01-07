760350ad56ebfd10b83e8cc3f7913c218159c397
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-08 00:11:37 +0900

    session9：配列とループ文

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/070_反復処理/030_配列とループ文/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/070_反復処理/030_配列とループ文/start/main.js
@@ -1,1 +1,15 @@
 const arry = [1, 2, 3, 4, 5];
+
+// for(i = 0; i < arry.length; i++) {
+//   console.log(arry[i]);
+// }
+
+// let i = 0;
+// while(i < arry.length) {
+//   console.log(arry[i]);
+//   i++;
+// }
+let v, i = 0;
+while(v = arry[i++]) {
+  console.log(v);
+}
