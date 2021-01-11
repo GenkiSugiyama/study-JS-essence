d3e8280dcfee010df8472b52b84927345f4c43c6
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-12 08:58:48 +0900

    session10：同期処理と非同期処理

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/010_同期処理と非同期処理/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/010_同期処理と非同期処理/start/main.js
@@ -1,3 +1,6 @@
+// JavaScriptはメインスレッドで実行される
+// 同期処理：メインスレッドでコードが順番に実行される。一つの処理が完了するまで絶対に次の処理には進まない
+// 非同期処理：メインスレッドから一時的に処理が切り離される。切り離される分、他の処理が実行できることになる
 function sleep(ms) {
   const startTime = new Date();
   while (new Date() - startTime < ms);
@@ -8,3 +11,7 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function(){
   console.log('button clicked');
 });
+
+setTimeout(function(){
+  sleep(3000);
+}, 3000);
