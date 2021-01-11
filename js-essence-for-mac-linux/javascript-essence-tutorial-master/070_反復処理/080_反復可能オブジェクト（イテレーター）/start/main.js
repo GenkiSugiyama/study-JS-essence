a11ce2209fb3ea2db05da9f0b285a1c93b6152d9
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-11 19:35:55 +0900

    session9：反復可能オブジェクト（イテレーター）

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
@@ -1,10 +1,38 @@
-const obj = {
+const items = {
 	prop1: 'value1',
 	prop2: 'value2',
 	prop3: 'value3'
 }
+// // 通常オブジェクトは一度配列に組みなおしてからでないとforof内で使えない
+// const items = Object.entries(obj);
+// for (let item of items) {
+// 	console.log(item);
+// }
 
-const items = Object.entries(obj);
+// Objectのprototypeに[Symbol.iterator]を追加することで反復可能オブジェクトとなる
+Object.prototype[Symbol.iterator] = function() {
+	// 繰り返し処理を行うオブジェクトのキー全てを配列として取得する
+	const keys = Object.keys(this);
+	console.log(this)
+	// カウント用の変数を定義
+	let i = 0;
+	// この関数の戻り値であるnextメソッド内でこの関数の呼び出し元であるオブジェクトをthisとして使用するための定義
+	// next関数内で普通にthisを使うとnext関数が呼び出し元となるので×
+	let _this = this;
+	return {
+		next() {
+			console.log(this);
+			let key = keys[i];
+			i++;
+			return {
+				done: i > keys.length,
+				value: [key, _this[key]],
+			}
+		}
+	}
+}
+
+// const items = Object.entries(obj);
 for (let item of items) {
 	console.log(item);
 }
