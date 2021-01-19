d853c3e3bfa97365557ddc13a4fc7be8b5e7395a
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-20 00:06:02 +0900

    seeesion10：Promiseチェーン

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/060_Promiseチェーン/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/060_Promiseチェーン/start/main.js
@@ -1,6 +1,25 @@
-function sleep(callback, val) {
-  setTimeout(function() {
-    console.log(val++);
-    callback(val);
-  }, 1000);
+function sleep(val) {
+  // returnにPromiseのインスタンスが渡されている
+  // Promiseインスタンス内のコールバック関数ではsetTImeoutが呼ばれているのでこのコールバック関数の処理が非同期処理として
+  return new Promise(function(resolve, reject){
+    console.log('テスト')
+    setTimeout(function() {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
+  })
 }
+
+sleep(0).then((val) => {
+  // Promiseを使って非同期処理をチェーンする場合はthenメソッド内のコールバック関数のreturnにPromiseのインスタンスを渡す
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+})
+
