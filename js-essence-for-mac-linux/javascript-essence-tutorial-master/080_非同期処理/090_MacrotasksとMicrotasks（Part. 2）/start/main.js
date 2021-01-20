ed082d7d79b785d6a07ba67b9763873f913ff6ff
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-20 23:39:34 +0900

    session10：MacroTasksとMicroTasks(Part2)

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/090_MacrotasksとMicrotasks（Part. 2）/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/090_MacrotasksとMicrotasks（Part. 2）/start/main.js
@@ -8,6 +8,17 @@ new Promise(function promise(resolve) {
 
 }).then(function job1() {
   console.log('job1');
+  setTimeout(function task2(){
+    console.log('task2')
+  });
+
+  queueMicrotask(function task4(){
+    console.log("job4");
+  })
+}).then(function job2() {
+  console.log('job2');
+}).then(function job3() {
+  console.log('job3');
 })
 
 console.log('global end');
