e1e111c71629ce29de552e786b778d14a2da31a1
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-20 23:39:11 +0900

    session10：MacroTasksとMicroTasks(Part1)

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
@@ -2,6 +2,8 @@ setTimeout(function task1() {
   console.log('task1');
 });
 
+// promise内のコールバック関数は同期的に処理される
+// then内のコールバック関数は非同期処理
 new Promise(function promise(resolve) {
   console.log('promise');
   resolve();
@@ -10,3 +12,9 @@ new Promise(function promise(resolve) {
 });
 
 console.log('global end');
+
+// MacroTask（タスクキュー）とMicroTask（ジョブキュー）が存在する場合、MicroTaskが優先的に処理される
+// MacroTaskはループ毎に1つずつしかタスクを実行しないが、MicroTaskはループ毎に積まれている全てのタスクを実行する
+
+// MacroTaskの主な物：setTimeout / setInterval / requestAnimationFrame
+// MicroTaskの主な物：Promises
