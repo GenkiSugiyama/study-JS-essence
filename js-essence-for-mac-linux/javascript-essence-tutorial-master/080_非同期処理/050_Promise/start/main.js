6fb66d5182ae4208a7f09eea539c8164c2d94a8e
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-22 01:02:13 +0900

    session10：AsyncとAwait

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/050_Promise/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/050_Promise/start/main.js
@@ -1,30 +1,34 @@
 // new Promise(コールバック関数(resolve, reject){}).then().catch().finally()
 
 // Promiseインスタンスの引数にはresolveとrejectを引数にもったコールバック関数を渡す
-new Promise(function(resolve, reject){
-  console.log('promise');
-  // resolve("hello"); // Promiseに渡したコールバック関数内でresolveを実行するとthenメソッド内の処理が呼ばれる
-  reject("bye"); // rejectを実行するとcatchメソッド内の処理が呼ばれる
-}).then(function(data){ //resolve実行時の引数が渡される
-  console.log("then：" + data);
-  return data; // 2つめのthenにresoleveの引数を渡したい場合はreturnでその引数を返してあげる
-  throw new Error("error"); // thenメソッド内からcatchメソッドに移行したい場合はthrow命令を使う。1つめのthenからcatchへ移行するため2つめのthenは呼ばれない（飛ばされる）
-}).then(function(data){ //resolve実行時の引数が渡される
-  console.log('then1：' + data);
-}).catch(function(data){ //reject実行時の引数が渡される
-  console.log("catch:" + data);
-}).finally(function(){ // finallyメソッドは必ず呼ばれる（引数を取れない）
-  console.log('finally');
-})
+// new Promise(function(resolve, reject){
+//   console.log('promise');
+//   // resolve("hello"); // Promiseに渡したコールバック関数内でresolveを実行するとthenメソッド内の処理が呼ばれる
+//   reject("bye"); // rejectを実行するとcatchメソッド内の処理が呼ばれる
+// }).then(function(data){ //resolve実行時の引数が渡される
+//   console.log("then：" + data);
+//   return data; // 2つめのthenにresoleveの引数を渡したい場合はreturnでその引数を返してあげる
+//   throw new Error("error"); // thenメソッド内からcatchメソッドに移行したい場合はthrow命令を使う。1つめのthenからcatchへ移行するため2つめのthenは呼ばれない（飛ばされる）
+// }).then(function(data){ //resolve実行時の引数が渡される
+//   console.log('then1：' + data);
+// }).catch(function(data){ //reject実行時の引数が渡される
+//   console.log("catch:" + data);
+// }).finally(function(){ // finallyメソッドは必ず呼ばれる（引数を取れない）
+//   console.log('finally');
+// })
 
 new Promise(function(resolve, reject){
   console.log('promise');
   setTimeout(function(){
-    resolve(0);
+    // resolve(0);
+    console.log('promise setTImeout')
+    reject(0)
   }, 1000);
 }).then(function(data){
   console.log(data)
   return data
+}, function(val){
+  console.log(val);
 }).finally(function(){
   console.log('終わり');
 })
--- js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/100_AwaitとAsync/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/100_AwaitとAsync/start/main.js
@@ -1,3 +1,26 @@
+// Async：Promiseを返却する関数を宣言する
+// Await：Promiseを返却する関数(Async)の非同期処理が完了するまで待つ
+// function sleep(val) {
+//   return new Promise(function(resolve) {
+//     setTimeout(function() {
+//       console.log(val++);
+//       resolve(val);
+//     }, 1000);
+//   });
+// }
+
+// sleep(0).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// })
+
 function sleep(val) {
   return new Promise(function(resolve) {
     setTimeout(function() {
@@ -7,14 +30,33 @@ function sleep(val) {
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-})
+// async関数自体もPromiseのインスタンスとなっている（thenメソッドで処理をつなげることもできる）
+async function init() {
+
+  // awaitはasync関数内でしか使用できない、awaitで非同期処理を待っている間でもasync関数外の同期処理は停止せずに処理が進む
+  let val = await sleep(0);
+  // ↑awaitをつけた関数はその関数がreturnで返すPromiseインスタンス内のコールバック関数内のresolveの実引数を戻り値として返す
+  // →上記のvalにはsleep関数実行時のresolveの引数であるvalの値が渡される
+  console.log(val);
+  console.log('hello'); // await式は非同期、awaitより下のconsole.logは同期処理だがawait式は非同期処理を実行して完了しするまで次の行を実行しないためconsole.logはawait式の後に実行される
+  // →await式を使うことで通常の同期処理のような流れでコードを書くことができる
+  return val; // async関数内で返却される値がthenのコールバック関数の引数として使用可能
+}
+
+init().then(function(val){
+  setTimeout(console.log(val), 1000);
+  
+});
+
+Promise.resolve(1).then((value) => {
+  console.log(value); // => 1
+  return value * 2;
+}).then(value => {
+  console.log(value); // => 2
+  return value * 2;
+}).then(value => {
+  console.log(value); // => 4
+  // 値を返さない場合は undefined を返すのと同じ
+}).then(() => {
+  console.log(value); // => undefined
+});
