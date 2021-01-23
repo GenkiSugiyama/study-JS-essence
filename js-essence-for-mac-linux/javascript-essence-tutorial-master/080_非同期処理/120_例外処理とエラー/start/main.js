4405da3c6c19a1580d9a43cab3f7d8642c296e24
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-23 21:04:26 +0900

    session10：例外処理とエラー

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/120_例外処理とエラー/start/main.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/080_非同期処理/120_例外処理とエラー/start/main.js
@@ -9,3 +9,33 @@
 // }
 
 // fetchUsers();
+
+async function fetchUsers() {
+  const response = await fetch('users.json');
+  // console.log(response);
+  if(response.ok) {
+    const json = await response.json();
+    // console.log(json.length);
+    if(!json.length) {
+      throw new Error("no data found");
+    }
+    return json;
+  }
+  
+}
+
+async function init() {
+  try{
+    const users = await fetchUsers();
+    for(const user of users) {
+      console.log(`I'm ${user.name}, ${user.age} years old`)
+    }
+  } catch(e) {
+    console.log(e);
+  } finally {
+    console.log('bye')
+  }
+  
+}
+init();
+
