a93816ef05692cfa139d8a1c7729d41909db98be
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-24 22:01:43 +0900

    session11：【修了確認】モジュール化

--- js-essence-for-mac-linux/javascript-essence-tutorial-master/090_モジュラーJavaScript/910_修了確認_モジュール化/start/calc.js
+++ js-essence-for-mac-linux/javascript-essence-tutorial-master/090_モジュラーJavaScript/910_修了確認_モジュール化/start/calc.js
@@ -3,7 +3,7 @@
  * 以下の即時関数をモジュール化してください。
  * ※calcオブジェクトはmain.js内で行い、実行してください。
  */
-const calc = (function() {
+export const calc = (function() {
 
     let val = 0;
 
@@ -30,4 +30,31 @@ const calc = (function() {
         },
     }
 })();
+
+// export const calc = function(){
+//     let val = 0;
+
+//     return {
+//         plus(target) {
+//             const newVal = val + target;
+//             console.log(`${val} + ${target} = ${newVal}`);
+//             val = newVal;
+//         },
+//         minus: function(target) {
+//             const newVal = val - target;
+//             console.log(`${val} - ${target} = ${newVal}`);
+//             val = newVal;
+//         },
+//         multiply: function(target) {
+//             const newVal = val * target;
+//             console.log(`${val} x ${target} = ${newVal}`);
+//             val = newVal;
+//         },
+//         divide: function(target) {
+//             const newVal = val / target;
+//             console.log(`${val} / ${target} = ${newVal}`);
+//             val = newVal;
+//         },
+//     }
+// }
 
