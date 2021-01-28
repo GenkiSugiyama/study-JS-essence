5392808c4ce7af00672607817444858ffef8e318
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-29 00:29:34 +0900

    Ajax通信：ユーザーIDを変更できるようにする

--- JSPrimer/ajaxapp/index.js
+++ JSPrimer/ajaxapp/index.js
@@ -2,7 +2,7 @@ console.log("index.js: loaded");
 
 // githubからユーザー情報を取得するための処理
 // githubが用意しているAPIにfetchAPIを使ってアクセスする
-const userId = 'GenkiSugiyama';
+// const userId = 'GenkiSugiyama';
 
 // HTTP通信を行うためのfetchAPIを使うことで引数に渡すURLに対してリクエストを行う
 // fetchはPromiseを返すので通信成功時はthen()で、エラー時はcatchで繋げられる
@@ -12,6 +12,7 @@ const userId = 'GenkiSugiyama';
 // Asyncfunctionへの書き換え
 async function main(){
   try{
+    const userId = getUserId();
     const userInfo = await fetchUserInfo(userId);
     const view = createView(userInfo);
     displayView(view)
@@ -24,23 +25,20 @@ async function main(){
 function fetchUserInfo(userId){
   return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
   .then((response) => {
-    console.log(response.status);
+    console.log(response);
     if(!response.ok){
+      // response.okプロパティがtrueの場合はerrorをそのまま返せないのでthenメソッド内でべっとエラーのPromiseオブジェクトを返す
       console.error("エラーレスポンス", response);
       return Promise.reject(new Error(`${response.status}：${response.statusText}`));
     } else {
       return response.json()
-      // fetchAPIが返すresponseの中身をjsonメソッドで取り出したjsonオブジェクトだけ返す
-      // .then((userInfo) => {
-      //   console.log(userInfo);
-      //   // APIで取得した要素をHTMLに表示させる
-      //   // HTMLの組み立てを関数化
-      //   const view = createView(userInfo);
-      //   // HTMLの描画も関数化
-      //   displayView(view);
-      // });
     }
   });
+}
+
+// inputタグからユーザーIDを取得する処理
+function getUserId() {
+  return document.getElementById('userId').value;
 }
 
 function createView(userInfo){
