7143dae7b46df45a3e42b7f2cd9bfdee20037eb2
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-01-29 09:12:43 +0900

    Nodecli：commanderの使い方

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,5 +1,10 @@
 // CommonJSモジュールではrequire関数にインポートしたいモジュールのファイルバスを渡す
 // require関数ではnpmでインストールしたパッケージ名で指定も可能（npmでインストールしたパッケージはnode_moduleディレクトリ配下にある）
-const myModule = require("./my-module");
 const program = require("commander");
-console.log(myModule.foo);
+
+// process.argvでコマンドライン引数へアクセス、それをparseメソッドに渡す
+// parseメソッドでコマンドライン引数をパースした結果を取り出す（program.args内に配列の要素として格納される）
+program.parse(process.argv);
+
+const filePath = program.args[0];
+console.log(filePath);
