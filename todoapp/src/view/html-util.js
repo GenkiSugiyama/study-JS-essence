3871a81f7b965c873e1581ec9e2dfe5f50760bcf
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-02-04 00:09:52 +0900

    Todoリスト：Todoアイテムの追加を実装する

--- todoapp/src/view/html-util.js
+++ todoapp/src/view/html-util.js
@@ -7,9 +7,27 @@ export function escapeSpecialCharas(str) {
     .replace(/'/g, "&#039;");
 }
 
+// HTML文字列からHMTL要素を作成して返す
 export function htmlToElement(html) {
   const template = document.createElement("template");
   template.innerHTML = html;
   return template.content.firstElementChild;
+}
+
+export function element(strings, ...values) {
+  const htmlString = strings.reduce((result, str, i) => {
+    const value = values[i-1];
+    if (typeof value === "string") {
+      return result + escapeSpecialCharas(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+  return htmlToElement(htmlString);
+}
+
+export function render(bodyElement, containerElement) {
+  containerElement.innerHTML = "";
+  containerElement.appendChild(bodyElement);
 }
 
