8e192276227fa28bef3760c5904704033cc34b8a
GenkiSugiyama <sgym0318marcos@gmail.com>
2021-02-03 23:52:09 +0900

    Todoリスト：入力内容をコンソールに表示する

---
+++ todoapp/src/view/html-util.js
@@ -0,0 +1,15 @@
+export function escapeSpecialCharas(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+export function htmlToElement(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.firstElementChild;
+}
+
